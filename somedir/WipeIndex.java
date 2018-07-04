0b87fcffee39881332063bdf61b9cc7600972ffd
Bogdan <dumitrumihaibogdan@gmail.com>
2018-07-05 02:20:01 +0300

    test

---
+++ somedir/WipeIndex.java
@@ -0,0 +1,27 @@
+package ro.orbuculum.rules;
+
+import java.io.File;
+import java.lang.ProcessBuilder.Redirect;
+
+import org.junit.rules.ExternalResource;
+
+/**
+ * Removes indexed documents.
+ * 
+ * @author bogdan_dumitru
+ */
+public class WipeIndex extends ExternalResource {
+  @Override
+  protected void before() throws Throwable {
+    ProcessBuilder pb = new ProcessBuilder()
+        .command("bash", "delete-all-documents.sh")
+        .directory(new File("../../solr"))
+        .inheritIO()
+        .redirectOutput(Redirect.PIPE);
+    pb.start().waitFor();
+    
+    System.out.println("Index wiped");
+    
+    super.before();
+  }
+}
